c2309c094e8f546959ba493279cbb6728385ed6b
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-28 01:23:49 +0200

    .add app bar login screen & test deve branche

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -7,6 +7,10 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Screen'),
+        centerTitle: true,
+      ),
       body: Center(
         child: Text('Login Screen'),
       ),
